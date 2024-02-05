12245d527a05dcc49983db3867950e562019df3c
Ammarhani1151992 <ammar.kamil1905@coeng.uobaghdad.edu.iq>
2024-02-05 22:33:18 +0300

    updated cors

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const tag = require("./routes/tag")
 const user =  require("./routes/user")
 const order = require("./routes/order")
 const wish = require("./routes/wishlist")
+const cors = require('cors');
+
 
 const port = 3000;
 
+app.use(cors());
 app.use(express.json())
 app.use(fileUpload({
     limits: {fileSize: 50 * 1024 * 124}
