959c186c6421b76274ad8b4c003dc9a6387a4d52
Ammarhani1151992 <ammar.kamil1905@coeng.uobaghdad.edu.iq>
2024-02-07 20:52:14 +0300

    solve image issue

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json())
 app.use(fileUpload({
-    limits: {fileSize: 200 * 1024 * 1024}
+    limits: {fileSize: 10000 * 1024 * 1024}
 }))
 
 app.use("/product", product);
