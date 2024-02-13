761698d5913ded5e198affb04e1afa77c0305b9b
Ammarhani1151992 <ammar.kamil1905@coeng.uobaghdad.edu.iq>
2024-02-13 23:11:07 +0300

    update database

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,11 @@ const express = require("express");
 const route = express.Router()
 const auth = require("../middleware")
 
-const {register, login, userView, AdminView} = require("../model/user")
+const {register, login, userView, adminView} = require("../model/user")
 
 route.post("/register", register)
 route.post("/login", login)
-route.get("/view", auth, AdminView)
+route.get("/view", auth, adminView)
 route.get("/userView", auth, userView)
 
 
